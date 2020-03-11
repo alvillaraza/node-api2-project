789f2787bf60c1f2cee3e2c85c6c680b17560d61
alexis villaraza <alexis.borja@gmail.com>
2020-03-10 17:14:07 -0700

    able to get post by id

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -19,6 +19,24 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  Hubs.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hub"
+      });
+    });
+});
+
 //post posts
 router.post('/', (req, res) => {
 
