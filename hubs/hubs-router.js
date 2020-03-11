4cdc0f0abd1e2e8ca50b0d265c7385e06595c1d2
alexis villaraza <alexis.borja@gmail.com>
2020-03-10 20:35:29 -0700

    able to post

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -1,10 +1,62 @@
-const express = require('express');
-const Hubs = require('../data/db.js');
+const express = require("express");
+const Hubs = require("../data/db.js");
 
 const router = express.Router();
-let posts = [];
 
+//create post
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  const userPost = req.body;
 
+  // res.status(201).json(userPost);
+
+  Hubs.insert(userPost)
+    .then(post => {
+      if (!title || !contents) {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the post.",
+          title: title,
+          contents: contents
+        });
+      }
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the hub"
+      });
+    });
+});
+
+//create comment by post id
+router.post("/:id/comments", (req, res) => {
+  const { title, content } = req.body;
+  const userPost = req.body;
+
+  if (!title || !content) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+
+  res.status(201).json(userPost);
+
+  // Hubs.insert(userPost)
+  //   .then(post => {
+  //     res.status(201).json(post);
+  //   })
+  //   .catch(error => {
+  //     // log error to database
+  //     console.log(error);
+  //     res.status(500).json({
+  //       message: "Error adding the hub"
+  //     });
+  //   });
+});
+
+//get posts
 router.get("/", (req, res) => {
   Hubs.find()
     .then(post => {
@@ -14,18 +66,21 @@ router.get("/", (req, res) => {
       // log error to database
       console.log(error);
       res.status(500).json({
-        error: "The posts information could not be retrieved." 
+        error: "The posts information could not be retrieved."
       });
     });
 });
 
+//get post by id
 router.get("/:id", (req, res) => {
   Hubs.findById(req.params.id)
     .then(post => {
       if (post) {
         res.status(200).json(post);
       } else {
-        res.status(404).json({ message: "The post with the specified ID does not exist." });
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
       }
     })
     .catch(error => {
@@ -37,9 +92,67 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//post posts
-router.post('/', (req, res) => {
+//get comment by post id
+router.get("/:id/comment", (req, res) => {
+  Hubs.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hub"
+      });
+    });
+});
 
+//deletes post by id
+router.delete("/:id", (req, res) => {
+  Hubs.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(post); //returns the number of records deleted?
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub"
+      });
+    });
+});
+
+//update post
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+    .then(updatePost => {
+      if (updatePost) {
+        res.status(200).json(updatePost);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified."
+      });
+    });
 });
 
 module.exports = router;
